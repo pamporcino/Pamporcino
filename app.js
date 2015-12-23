e935c1f473469f45df37feaf9a45ae19af83b167
Rose Deneau <rdenea2119@gmail.com>
2015-12-23 01:40:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 var main = function () {
   $('.icon-menu').click();
-}
+};
+  $('.menu').animate({
+    left: '0px'
+  } , 200);
+  
+  $('.body').animate({
+  }, 200);
+  });
+  };
+  $('.icon-close').click(function(){
+    $('.menu').animate({
+      left: "-285px"
+    } , 200);
+    
+    $('.body').animate({
+      left: "0px"
+    });
+
 <script>
   $(document).ready(function () {
         $("div.header").fadeIn(1000);
