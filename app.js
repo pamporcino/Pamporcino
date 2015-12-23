84f06aa109251329252842f84fbf665d5d7afda1
Rose Deneau <rdenea2119@gmail.com>
2015-12-23 01:04:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var main = function () {
   $('.icon-menu').click();
 }
-
+<script>
+  $(document).ready(function () {
+        $("div.header").fadeIn(1000);
+        
+    });
+</script>
   $(document).ready(main);
